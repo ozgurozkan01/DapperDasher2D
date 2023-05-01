004d0195ed8fd41d817c5830a183691c50765f4a
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-05-01 19:09:09 +0300

    add obstacle

---
+++ Nebula.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 5/1/2023.
+//
+
+#include "Nebula.h"
---
+++ Nebula.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 5/1/2023.
+//
+
+#ifndef MAIN_CPP_NEBULA_H
+#define MAIN_CPP_NEBULA_H
+
+
+class Nebula {
+
+};
+
+
+#endif //MAIN_CPP_NEBULA_H
