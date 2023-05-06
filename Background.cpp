6cbb97ef177a8e852eda53c7f255f8c25774e697
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-05-06 22:05:49 +0300

    add background class

---
+++ Background.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 5/6/2023.
+//
+
+#include "Background.h"
---
+++ Background.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 5/6/2023.
+//
+
+#ifndef MAIN_CPP_BACKGROUND_H
+#define MAIN_CPP_BACKGROUND_H
+
+
+class Background {
+
+};
+
+
+#endif //MAIN_CPP_BACKGROUND_H
