0d52e36ec009f136807e460b7b668c7030960726
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-05-08 01:07:13 +0300

    add coin class

---
+++ Coin.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 5/8/2023.
+//
+
+#include "Coin.h"
---
+++ Coin.h
@@ -0,0 +1,19 @@
+//
+// Created by ozgur on 5/8/2023.
+//
+
+#ifndef MAIN_CPP_COIN_H
+#define MAIN_CPP_COIN_H
+
+
+class Coin {
+
+private:
+    
+
+public:
+
+};
+
+
+#endif //MAIN_CPP_COIN_H
